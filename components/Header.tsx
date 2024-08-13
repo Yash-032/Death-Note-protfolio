fc12ad7d98b871eb2ca03edb635b70312d7436d4
Yash Agarwal <yashag0910@gmail.com>
2024-08-14 03:09:44 +0530

    Basic Protfolio Setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-black p-4">
+    <header className="bg-black p-5">
       <nav>
-        <ul className="flex justify-center space-x-8">
-          <li><Link href="/"><div className="text-light hover:text-redAccent font-cinzel">Home</div></Link></li>
-          <li><Link href="/about"><div className="text-light hover:text-redAccent">About</div></Link></li>
-          <li><Link href="/projects"><div className="text-light hover:text-redAccent">Projects</div></Link></li>
-          <li><Link href="/contact"><div className="text-light hover:text-redAccent">Contact</div></Link></li>
+        <ul className="flex justify-center space-x-12">
+          <li><Link href="/"><div className="text-light text-2xl hover:text-redAccent font-cinzel">Home</div></Link></li>
+          <li><Link href="/about"><div className="text-light text-2xl hover:text-redAccent">About</div></Link></li>
+          <li><Link href="/about"><div className="text-light text-2xl hover:text-redAccent">Skills</div></Link></li>
+          <li><Link href="/projects"><div className="text-light text-2xl hover:text-redAccent">Projects</div></Link></li>
+          <li><Link href="/contact"><div className="text-light text-2xl hover:text-redAccent">Contact</div></Link></li>
         </ul>
       </nav>
     </header>
