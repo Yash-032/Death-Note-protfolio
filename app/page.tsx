9332e2a095ecea23c7c607f0c8005554d98068b2
Yash Agarwal <yashag0910@gmail.com>
2024-08-14 16:29:21 +0530

    Background Image added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image';
+import bgImage from '@/public/ryuk-death-note-ai-2560x1440-14431.jpg';
+
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-5xl text-redAccent mb-4">Welcome to My Portfolio</h1>
-      <p className="text-xl text-light">Inspired by Death Note</p>
+    <section
+      className="relative flex flex-col items-center justify-center h-screen text-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImage.src})` }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10">
+        <h1 className="text-5xl text-redAccent mb-4">Welcome to My Portfolio</h1>
+        <p className="text-xl text-light">Inspired by Death Note</p>
+      </div>
     </section>
   );
 }
+
